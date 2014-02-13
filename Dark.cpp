3d543ddf374402ad682abdee9955a355aa1844e0
Amir_Cinderella <irancore@users.noreply.github.com>
2014-02-14 03:25:23 +0330

    Update Dark.cpp

--- Dark.cpp
+++ Dark.cpp
@@ -5,3 +5,37 @@ SDComment:
 SDCategory: Custom boss Dark
 EndScriptData
 */
+
+//Yell Haye Boss
+
+#define SAY_MORDAN_BOSS_1 "Matne Entekhabi 1 ..."
+#define SAY_MORDAN_BOSS_2 "Matne Entekhabi 2 ..."
+#define SAY_ENTER_COMBAT_1 "Matne Entekhabi 1 ..."
+#define SAY_ENTER_COMBAT_2 "Matne Entekhabi 2 ..."
+
+
+#include "ScriptPCH.h"
+class boss_dark() : public Creaturescript
+{
+public:
+	boss_dark CreatureScript("npc_test")	{ }
+
+	struct boss_darkAI: public scriptedAI
+	{
+	  npc_testAI(Creature * c) : ScriptedAI(c) { }
+	  
+	  		void reset()
+		{
+    me->RestoreDisplayId();
+		}
+		
+				void JustDied()
+		{
+		}
+		
+		void EnterCombat()
+    {
+    me->MonsterYell(SAY_ENTER_COMBAT_1, LANG_UNIVERSAL, 0);
+    }
+		
+	};
